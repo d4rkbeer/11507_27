263e4d22b0566f2948cf30e02f64d276b539519e
d4rkbeer <dinarsubhanov123@gmail.com>
2026-05-23 18:20:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Concurrent;
+using System.Reflection;
+
+namespace ControlWork
+{
+
+    public static class TypeFactory
+    {
+        public static object CreateAndFill(Type type, Dictionary<string, object> values)
+        {
+            object obj = Activator.CreateInstance(type);
+
+            foreach (var prop in type.GetProperties())
+            {
+                if (prop.CanWrite && values.TryGetValue(prop.Name, out object value))
+                {
+                    try
+                    {
+                        prop.SetValue(obj, Convert.ChangeType(value, prop.PropertyType));
+                    }
+                    catch { }
+                }
+            }
+            return obj;
+        }
+    }
+
+    
+}
+
+
+
